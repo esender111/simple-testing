84e4892801627bfd9a1965e0f5dcbf0a043dc99a
Esender Dubashev <esender.dubashev@cgi.com>
2022-01-28 16:15:46 -0500

    new java project

---
+++ src/test/java/com/cgi/AppTest.java
@@ -0,0 +1,24 @@
+
+package com.cgi;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
